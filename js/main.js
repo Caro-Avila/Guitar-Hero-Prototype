eaaf80351e8c3fc295f9bacf65e34836c8de5b5e
juliana <carolinasanchez080205@gmail.com>
2024-03-13 19:02:30 -0500

    Third commit
    Added notes and made start button work

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     const startButton = document.getElementById('startButton');
     const pauseButton = document.getElementById('pauseButton');
+    const areaCirculos = document.querySelector('.contenedor'); // Selección del área de los círculos
+    var intervaloCantidad;
 
     // Añadimos un evento de clic al botón de inicio
     startButton.addEventListener('click', () => {
@@ -24,13 +26,39 @@ document.addEventListener("DOMContentLoaded", function() {
 
     // Función para iniciar el juego
     function iniciarJuego() {
-        // Aquí iría la lógica para comenzar el juego
-        console.log('Juego iniciado');
+        // Intervalo para generar cuadrados
+        intervaloCantidad = setInterval(function () {
+            crearNota();
+        }, 1000);
     }
 
     // Función para pausar el juego
     function pausarJuego() {
-        // Aquí iría la lógica para pausar el juego
-        console.log('Juego pausado');
+        // Detener el intervalo de generación de cuadrados
+        clearInterval(intervaloCantidad);
+
+    }
+
+    // Función para crear cuadrado
+    function crearNota() {
+        var nuevaNota = document.createElement("div");
+        nuevaNota.setAttribute("class", "nota");
+        nuevaNota.dataset.velocidad = (Math.random() * 10);
+        nuevaNota.style.left = Math.floor(Math.random() * (areaCirculos.offsetWidth - nuevaNota.offsetWidth)) + "px";
+        areaCirculos.appendChild(nuevaNota);
+        moverNota(nuevaNota);
+    }
+
+    // Función para mover cuadrado
+    function moverNota(nota) {
+        var contador = 0;
+        var intervalo = setInterval(function () {
+            contador = contador + 1;
+            nota.style.top = (contador * nota.dataset.velocidad) + "px";
+            if (contador * nota.dataset.velocidad > areaCirculos.offsetHeight) {
+                nota.remove(); // Eliminar cuadrado cuando sale del área
+                clearInterval(intervalo); // Detener intervalo
+            }
+        }, 10);
     }
 });
